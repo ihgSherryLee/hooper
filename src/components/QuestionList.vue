3d548e5c3ecb8bd212b4dfbce0dae7ed482dac15
ihgSherryLee <lxyihggyml@163.com>
2016-03-24 00:11:38 +0800

    add question list

---
+++ src/components/QuestionList.vue
@@ -0,0 +1,85 @@
+<style lang="less">
+
+</style>
+
+<template>
+  <div class="main wrap">
+    <div class="content">
+      <div class="news">最新动态</div>
+      <div class="news-list-detail">
+        <span class="from-topic">来自<a href="#">XXX</a></span>
+        <p class="question-title">程序员都有哪些强迫症行为？</p>
+        <p calss="question-detail">我虽然不是程序员，偶尔写写代码，提升工作效率，但是写代码时，会强迫自己缩进，看到不缩进的代码，特别难受，喜欢强迫自己写注释，看到没注释的代码就想一把扔过去。还有的人写C语言强迫大括号换行，还有的人强迫自己用vim或者emacs编辑器，都有哪些强迫症？这些强迫症是否可以治疗？</p>
+        <div class="answer-detail">
+          <button class="agree"></button>
+          <button class="disagree"></button>
+          <p class="author">XXX</p>
+          <p class="answer">习惯按Ctrl+S⋯⋯有时候看网页看着看着就Ctrl+S一下，之后突然反应过来不对⋯⋯看到bug就想改⋯⋯即使不是自己的bug⋯⋯还喜欢找bug，看到登录框就想试试能不能SQL注入⋯⋯有时候还会抓包或者发假包⋯⋯看到一个软件就会想自己要做的话怎么做⋯⋯</p>
+          <p class="date">发布于 2016-01-15</p>
+          <div class="feed-meta">
+            <span class="comment">评论</span>
+          </div>
+        </div>
+      </div>
+      <div class="news-list-detail">
+        <span class="from-topic">来自<a href="#">XXX</a></span>
+        <p class="question-title">程序员都有哪些强迫症行为？</p>
+        <p calss="question-detail">我虽然不是程序员，偶尔写写代码，提升工作效率，但是写代码时，会强迫自己缩进，看到不缩进的代码，特别难受，喜欢强迫自己写注释，看到没注释的代码就想一把扔过去。还有的人写C语言强迫大括号换行，还有的人强迫自己用vim或者emacs编辑器，都有哪些强迫症？这些强迫症是否可以治疗？</p>
+        <div class="answer-detail">
+          <button class="agree"></button>
+          <button class="disagree"></button>
+          <p class="author">XXX</p>
+          <p class="answer">习惯按Ctrl+S⋯⋯有时候看网页看着看着就Ctrl+S一下，之后突然反应过来不对⋯⋯看到bug就想改⋯⋯即使不是自己的bug⋯⋯还喜欢找bug，看到登录框就想试试能不能SQL注入⋯⋯有时候还会抓包或者发假包⋯⋯看到一个软件就会想自己要做的话怎么做⋯⋯</p>
+          <p class="date">发布于 2016-01-15</p>
+          <div class="feed-meta">
+            <span class="comment">评论</span>
+          </div>
+        </div>
+      </div>
+      <div class="news-list-detail">
+        <span class="from-topic">来自<a href="#">XXX</a></span>
+        <p class="question-title">程序员都有哪些强迫症行为？</p>
+        <p calss="question-detail">我虽然不是程序员，偶尔写写代码，提升工作效率，但是写代码时，会强迫自己缩进，看到不缩进的代码，特别难受，喜欢强迫自己写注释，看到没注释的代码就想一把扔过去。还有的人写C语言强迫大括号换行，还有的人强迫自己用vim或者emacs编辑器，都有哪些强迫症？这些强迫症是否可以治疗？</p>
+        <div class="answer-detail">
+          <button class="agree"></button>
+          <button class="disagree"></button>
+          <p class="author">XXX</p>
+          <p class="answer">习惯按Ctrl+S⋯⋯有时候看网页看着看着就Ctrl+S一下，之后突然反应过来不对⋯⋯看到bug就想改⋯⋯即使不是自己的bug⋯⋯还喜欢找bug，看到登录框就想试试能不能SQL注入⋯⋯有时候还会抓包或者发假包⋯⋯看到一个软件就会想自己要做的话怎么做⋯⋯</p>
+          <p class="date">发布于 2016-01-15</p>
+          <div class="feed-meta">
+            <span class="comment">评论</span>
+          </div>
+        </div>
+      </div>
+      <div class="news-list-detail">
+        <span class="from-topic">来自<a href="#">XXX</a></span>
+        <p class="question-title">程序员都有哪些强迫症行为？</p>
+        <p calss="question-detail">我虽然不是程序员，偶尔写写代码，提升工作效率，但是写代码时，会强迫自己缩进，看到不缩进的代码，特别难受，喜欢强迫自己写注释，看到没注释的代码就想一把扔过去。还有的人写C语言强迫大括号换行，还有的人强迫自己用vim或者emacs编辑器，都有哪些强迫症？这些强迫症是否可以治疗？</p>
+        <div class="answer-detail">
+          <button class="agree"></button>
+          <button class="disagree"></button>
+          <p class="author">XXX</p>
+          <p class="answer">习惯按Ctrl+S⋯⋯有时候看网页看着看着就Ctrl+S一下，之后突然反应过来不对⋯⋯看到bug就想改⋯⋯即使不是自己的bug⋯⋯还喜欢找bug，看到登录框就想试试能不能SQL注入⋯⋯有时候还会抓包或者发假包⋯⋯看到一个软件就会想自己要做的话怎么做⋯⋯</p>
+          <p class="date">发布于 2016-01-15</p>
+          <div class="feed-meta">
+            <span class="comment">评论</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,51 +2,50 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 /* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
-})
 
 Vue.use(VueRouter)
-Vue.http.options.emulateJSON = true
+// Vue.http.options.emulateJSON = true
 var router = new VueRouter()
+
+import QuestionList from './components/QuestionList'
 router.map({
-    '/': {
-      component: QuestionList,
-      subRoutes: {
-        '/': {
-          component: QuestionList
-        },
-        '/details': {
-          component: QuestionArticle
-        }
-      }
-    },
-    '/edit/:mode': {
-      component: writePanel
-    },
-    '/search/tag/:tag': {
-      component: browseMode,
-      subRoutes: {
-        '/': {
-          component: blogList
-        }
-      }
-    },
-    '/search/time/:time': {
-      component: browseMode,
-      subRoutes: {
-        '/': {
-          component: blogList
-        }
-      }
-    },
-    '/search/title/:title': {
-      component: browseMode,
-      subRoutes: {
-        '/': {
-          component: blogList
-        }
-      }
-    },
+  '/': {
+    component: QuestionList
+    // subRoutes: {
+    //   '/': {
+    //     component: QuestionList
+    //   },
+    //   '/details': {
+    //     component: QuestionArticle
+    //   }
+    // }
+  }
+  // '/edit/:mode': {
+  //   component: writePanel
+  // },
+  // '/search/tag/:tag': {
+  //   component: browseMode,
+  //   subRoutes: {
+  //     '/': {
+  //       component: blogList
+  //     }
+  //   }
+  // },
+  // '/search/time/:time': {
+  //   component: browseMode,
+  //   subRoutes: {
+  //     '/': {
+  //       component: blogList
+  //     }
+  //   }
+  // },
+  // '/search/title/:title': {
+  //   component: browseMode,
+  //   subRoutes: {
+  //     '/': {
+  //       component: blogList
+  //     }
+  //   }
+  // },
 })
+router.start(App, 'app')
