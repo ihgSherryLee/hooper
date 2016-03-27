f768582f8e43e59fdabe739ec1707d7e874eb3eb
ihgSherryLee <lxyihggyml@163.com>
2016-03-28 00:20:14 +0800

    add login page

---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<style lang="less">
+  .wrap {
+    margin: auto;
+    padding: 40px 0;
+    width: 300px;
+  }
+  .title {
+    front-size: 24px;
+  }
+</style>
+
+<template>
+  <div class="wrap">
+    <div class="title">HOOPER</div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
