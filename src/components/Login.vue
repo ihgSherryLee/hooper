c706e797b63aed571008604e45385f4b1f919ef8
ihgSherryLee <lxyihggyml@163.com>
2016-03-29 00:13:09 +0800

    change login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,14 +5,54 @@
     width: 300px;
   }
   .title {
-    front-size: 24px;
+    font-size: 40px;
+    text-align: center;
+  }
+  .tab-nav {
+    margin-bottom: 10px;
+    font-size: 18px;
+    text-align: center;
+    .nav-slide a {
+      display: inline-block;
+      width: 4em;
+      line-height: 35px;
+    }
+  }
+  .view-signup {
+    input {
+      padding: 20px;
+      line-height: 19px;
+      border: none;
+      border-radius: 0;
+    }
+    .sign-btn {
+      margin-top: 20px;
+      width: 100%;
+      line-height: 41px;
+      border: none;
+      border-radius: 3px;
+      background: rgba(204,91,91,0.4);
+    }
   }
 </style>
 
 <template>
   <div class="wrap">
     <div class="title">HOOPER</div>
-    
+    <div class="tab-nav">
+      <div class="nav-slide">
+        <a href="#singup" class="active">注册</a>
+        <a href="#singin">登录</a>
+      </div>
+    </div>
+    <div class="view view-signup selected">
+      <form class="signup-form">
+        <input type="text" class="form-control" name="fullname" placeholder="姓名">
+        <input type="text" class="form-control" name="email" placeholder="邮箱">
+        <input type="password" class="form-control" placeholder="密码（不少于6位数字）">
+        <button class="sign-btn submit" type="submit">注册</button>
+      </form>
+    </div>
   </div>
 </template>
 
