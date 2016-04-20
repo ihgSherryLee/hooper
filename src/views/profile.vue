438dff255a70b46d37525a4bb645266d0eb31b94
ihgSherryLee <lxyihggyml@163.com>
2016-04-21 00:55:07 +0800

    change profile page

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -2,12 +2,18 @@
   .profile-inner {
     border: 1px solid #ccc;
     border-radius: 2px;
-    .profile-form {
+    .profile-item {
       padding: 24px 12px;
       border-bottom: 1px solid #ccc;
       clear: both;
+      span {
+        position: absolute;
+        display: inline-block;
+        width: 100px;
+        text-align: right;
+      }
       .profile-item-content {
-        float: left;
+        padding-left: 140px;
       }
     }
   }
@@ -17,46 +23,38 @@
   <globle-header></globle-header>
   <div class="main-wrap">
     <div class="main-content-inner profile-inner">
-      <div class="profile-form">编辑个人资料</div>
-      <div class="profile-form">
-        <div class="profile-item">
-          <span>头像</span>
-          <div class="profile-item-content">
-            <img src="">
+      <div class="profile-item">编辑个人资料</div>
+      <div class="profile-item">
+        <span>头像</span>
+        <div class="profile-item-content">
+          <img src="">
+        </div>
+      </div>
+      <div class="profile-item">
+        <span>性别</span>
+        <div class="profile-item-content">
+          <div v-show="!show.gender" class="profile-item-content-static">填写</div>
+          <div v-show="show.gender" class="profile-item-content-edit">
+            <input type="radio" name="gender">男
+            <input type="radio" name="gender">女
           </div>
         </div>
       </div>
-      <div class="profile-form">
-        <div class="profile-item">
-          <span>性别</span>
-          <div class="profile-item-content">
-            <div class="profile-item-content-static">填写</div>
-            <div class="profile-item-content-edit">
-              <input type="radio" name="gender">男
-              <input type="radio" name="gender">女
-            </div>
+      <div class="profile-item">
+        <span>一句话介绍</span>
+        <div class="profile-item-content">
+          <div v-show="!show.headline" class="profile-item-content-static">填写</div>
+          <div v-show="show.headline" class="profile-item-content-edit">
+            <input type="text" name="headline">
           </div>
         </div>
       </div>
-      <div class="profile-form">
-        <div class="profile-item">
-          <span>一句话介绍</span>
-          <div class="profile-item-content">
-            <div class="profile-item-content-static">填写</div>
-            <div class="profile-item-content-edit">
-              <input type="text" name="headline">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="profile-form">
-        <div class="profile-item">
-          <span>个人简介</span>
-          <div class="profile-item-content">
-            <div class="profile-item-content-static">填写</div>
-            <div class="profile-item-content-edit">
-              <textarea name="descrition"></textarea>
-            </div>
+      <div class="profile-item">
+        <span>个人简介</span>
+        <div v-show="!show.descrition" class="profile-item-content">
+          <div class="profile-item-content-static">填写</div>
+          <div v-show="show.descrition" class="profile-item-content-edit">
+            <textarea name="descrition"></textarea>
           </div>
         </div>
       </div>
@@ -70,6 +68,11 @@
   module.exports = {
     data: function () {
       return {
+        show: {
+          gender: true,
+          headline: false,
+          descrition: false
+        }
       }
     },
     methods: {
