f424327da4fb9d6f7f558df0eb26c5f2aea60235
sherry <lxyihggyml@163.com>
2016-04-21 19:12:00 +0800

    change profile page

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -6,7 +6,7 @@
       padding: 24px 12px;
       border-bottom: 1px solid #ccc;
       clear: both;
-      span {
+      .item-title {
         position: absolute;
         display: inline-block;
         width: 100px;
@@ -25,15 +25,15 @@
     <div class="main-content-inner profile-inner">
       <div class="profile-item">编辑个人资料</div>
       <div class="profile-item">
-        <span>头像</span>
+        <span class="item-title">头像</span>
         <div class="profile-item-content">
           <img src="">
         </div>
       </div>
       <div class="profile-item">
-        <span>性别</span>
+        <span class="item-title">性别</span>
         <div class="profile-item-content">
-          <div v-show="!show.gender" class="profile-item-content-static">填写</div>
+          <div v-show="!show.gender" class="profile-item-content-static"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>填写</div>
           <div v-show="show.gender" class="profile-item-content-edit">
             <input type="radio" name="gender">男
             <input type="radio" name="gender">女
@@ -41,18 +41,18 @@
         </div>
       </div>
       <div class="profile-item">
-        <span>一句话介绍</span>
+        <span class="item-title">一句话介绍</span>
         <div class="profile-item-content">
-          <div v-show="!show.headline" class="profile-item-content-static">填写</div>
+          <div v-show="!show.headline" class="profile-item-content-static"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>填写</div>
           <div v-show="show.headline" class="profile-item-content-edit">
             <input type="text" name="headline">
           </div>
         </div>
       </div>
       <div class="profile-item">
-        <span>个人简介</span>
+        <span class="item-title">个人简介</span>
         <div v-show="!show.descrition" class="profile-item-content">
-          <div class="profile-item-content-static">填写</div>
+          <div class="profile-item-content-static"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>填写</div>
           <div v-show="show.descrition" class="profile-item-content-edit">
             <textarea name="descrition"></textarea>
           </div>
